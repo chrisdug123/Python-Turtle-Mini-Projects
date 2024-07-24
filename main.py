e094cb03de644909a42057460a71ec0d1a9c975a
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-24 23:22:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+##Integer
+#age = 20
+##Float
+#temperature = 20.7
+##String
+##is_day = True
+
+#print(country)
+
+#country= False
+#print(country)
+
+#country_name="Australia"
+#population=25000000
+#landlocked=False
+#border_size=60000.256
+
+health = 100
+
+health = health - 20
+print(health)
+
+health = health + 50
+print(health)
+
+health = health * 1.5
+
+health = health / 2
+print(health)
