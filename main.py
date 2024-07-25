635d1156267cbfe7de3362c2ea783b7442e7b1e9
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-25 13:14:34 +0000

    Introduced to functions

--- main.py
+++ main.py
@@ -1,45 +1,8 @@
-##Integer
-#age = 20
-##Float
-#temperature = 20.7
-##String
-##is_day = True
 
-#print(country)
 
-#country= False
-#print(country)
 
-#country_name="Australia"
-#population=25000000
-#landlocked=False
-#border_size=60000.256
+def say_hello(name):
+  print(f"Hello " + name)
 
-#health = 100
 
-#health = health - 20
-#print(health)
-
-#health = health + 50
-#print(health)
-
-#health = health * 1.5
-
-#health = health / 2
-#print(health)
-
-a= 5
-b=8
-
-#if a ==b:
-#  print("a is equal to b")
-#else:
-#  print("a is not equal to b")
-
-if a>b:
-  print("a is greater than b")
-elif a<b:
-  print("a is less than b")
-else:
-  print("a is equal to b")
-  
+say_hello("Jonathan")
