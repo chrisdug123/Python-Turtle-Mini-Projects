731767466d0961dce1dbb5f9e401057ab8ed5163
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-29 20:43:26 +0000

    Finished starry night project

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from turtle import *
 import turtle
-
+from random import *
 t = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(1.0, 1.0)
+width=int(window_width())
+height=int(window_height())
+#100 stars, black background
+#random star size and location
+bgcolor("black")
+t.speed(40)
+def draw_star(width,height):
+  t.penup()
+  t.goto(randrange(round(-width/2),round(width /2)), randrange(round(-height/2),round(height/2)))
+  t.pendown()
+  t.color("white")
+  t.begin_fill()
+  t.circle(randrange(1,10))
+  t.end_fill()
 
-def move_and_turn(t,angle):
-  t.forward(50)
-  t.right(angle)
 
-for x in range(12):
-  move_and_turn(t,30)
+
+
+for x in range(100):
+  draw_star(width,height)
+  x=x+1
+  
 
 
 
